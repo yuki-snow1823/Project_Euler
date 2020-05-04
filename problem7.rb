1801a1886213ce18914250366b7b08499a42ca4e
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-04 09:55:29 +0900

    [finish]problem7 他の人の解答理解

--- problem7.rb
+++ problem7.rb
@@ -36,3 +36,26 @@ until primeNumbers.length == 10001 do
 end
 
 p primeNumbers.last
+
+
+# 他の人の解答
+
+# def prime?(number)
+#   (2..number-1).each do |n|
+#     if number%n == 0
+#       return false
+#     end
+#   end
+#   return true
+# end
+
+# number = 2
+# position = 1
+# until position == 10001
+#   number += 1
+#   if prime?(number)
+#     position += 1
+#   end
+# end
+
+# puts "Answer: #{number}"
