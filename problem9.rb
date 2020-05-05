9081fb08372e11d8f4006bbc7894e42bb66c9dad
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-06 08:19:41 +0900

    [finish]problem9 ピタゴラス

--- problem9.rb
+++ problem9.rb
@@ -1,15 +1,17 @@
 # ピタゴラス数(ピタゴラスの定理を満たす自然数)とは a < b < c で以下の式を満たす数の組である.
-
 # a^2 + b^2 = c^2
 # 例えば, 3^2 + 4^2 = 9 + 16 = 25 = 5^2 である.
-
 # a + b + c = 1000 となるピタゴラスの三つ組が一つだけ存在する.
 # これらの積 abc を計算しなさい.
 
-def pita(a, b, c)
-  if a*a + b*b = c*c
-    return true
+arr = []
+(1..333).each do |n|
+  (n + 1).upto(998) do |o|
+    (o + 1).upto(998) do |q|
+      if n + o + q == 1000 && n*n + o*o  == q*q 
+        arr.push(n, o, q)
+      end
+    end
   end
 end
-
-p 1.pita
+puts arr.inject(:*)
