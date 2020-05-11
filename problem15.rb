9ae3413ea37fcd3469cafb42114f57cf3cdb3a98
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-12 08:53:11 +0900

    [add]problem15 格子問題における最短経路 他の人の回答

--- problem15.rb
+++ problem15.rb
@@ -13,3 +13,17 @@ def factorial(number)
 end
 
 p factorial(40) / (factorial(20) * factorial(20)) == 137846528820
+
+# p array.inject(3,:*) #初期値3に対して、配列の要素をすべて掛ける
+
+def lattice_path_roots(lattice_number:)
+  total = lattice_number * 2
+  n = ((total - lattice_number + 1)..total).inject(:*)
+  r = (1..lattice_number).inject(:*)
+  return n / r
+end
+
+puts lattice_path_roots(lattice_number: 20)
+
+puts [*21..40].inject(:*) / [*1..20].inject(:*)
+
