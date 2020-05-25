f6aabddeb5acfafc68b98f07cea0eca793808bd4
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-25 15:43:42 +0900

    [add]problem26  逆数の循環節 他の人の解答

--- problem26.rb
+++ problem26.rb
@@ -29,3 +29,54 @@ end
 
 b = answer.map(&:to_s).sort_by(&:length).last
 p answer.map(&:to_s).index(b) + 1 == 993
+
+
+# 他の人の回答
+# http://tsumuji.cocolog-nifty.com/tsumuji/2009/07/project-euler-2.html
+
+def count(m)
+  m = m / 2 while m.even?
+  m = m / 5 while m.modulo(5).zero?
+  return 0 if m == 1
+  n = 10
+  c = 1
+  until n.modulo(m) == 1
+    n = n * 10
+    c = c + 1
+  end
+  return c
+end
+
+ 
+
+max = [0, 0]
+
+999.downto(2) do |i|
+
+  break if i < max[1]
+
+  c = count(i)
+
+  max = [i, c] if c > max[1]
+
+end
+
+ 
+
+puts max[0]
+
+# 基本的には分子をどんどん 10 倍していって分母で割ったときに余りが 1 になるところを探していきます。（余りが 1 になったところからまた繰り返しが始まるので……）
+
+
+# ただし、分母を素因数分解したときに、因子に 2 や 5 が含まれているとそのままの考え方では通用しなくなります。
+
+
+# そこで、循環節の長さを考える場合は、次のように分けて考えると分かりやすいと思います。
+
+
+
+# 分母 d が 2 または 5 の因子を含まない場合
+
+# 分母 d が 2 または 5 以外の因子を持たない場合
+
+# その他の場合
