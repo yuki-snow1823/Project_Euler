9c097c3b9fc4e80efb84ea2b77841b6b98035016
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-25 15:50:31 +0900

    [add]problem26 逆数の循環節 フェルマーの小定理を使った場合 要復習

--- problem26.rb
+++ problem26.rb
@@ -80,3 +80,25 @@ puts max[0]
 # 分母 d が 2 または 5 以外の因子を持たない場合
 
 # その他の場合
+
+
+max = 1000
+prime = Array.new(max, 1)
+prime[0..1] = [0, 0]
+i = 2
+while i < Math.sqrt(max).to_i + 1 do
+	(i + i).step(max, i){|x| prime[x] = 0}
+	i += prime[(i + 1)..max].index(1) + 1
+end
+
+puts prime.each_index{|i|
+	prime[i] = i unless prime[i].zero?
+}.select{|x| !x.zero?}.map{|x|
+	i = 1
+	while true
+		r = (10 ** i) % x
+		break if r == 1 || r == 0
+		i += 1
+	end
+	[x, r == 0 ? 0 : i]
+}.sort{|a, b| b[1] <=> a[1]}[0][0]
