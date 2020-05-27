0cf6601d0e28dfa4a7ae351f16940bf0e7bb5f51
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-27 20:25:33 +0900

    [review]10001番目の素数

--- problem7.rb
+++ problem7.rb
@@ -40,22 +40,22 @@ p primeNumbers.last
 
 # 他の人の解答
 
-# def prime?(number)
-#   (2..number-1).each do |n|
-#     if number%n == 0
-#       return false
-#     end
-#   end
-#   return true
-# end
+def prime?(number)
+  (2..number-1).each do |n|
+    if number%n == 0
+      return false
+    end
+  end
+  return true
+end
 
-# number = 2
-# position = 1
-# until position == 10001
-#   number += 1
-#   if prime?(number)
-#     position += 1
-#   end
-# end
+number = 2
+position = 1
+until position == 10001
+  number += 1
+  if prime?(number)
+    position += 1
+  end
+end
 
-# puts "Answer: #{number}"
+puts "Answer: #{number}"
