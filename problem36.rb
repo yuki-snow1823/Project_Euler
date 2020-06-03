44922fcc61fa508f63007852cce361e65542e5ff
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-04 08:52:00 +0900

    [add]problem36 二種類の基数による回文数 自分の解答+他の人の解凍

--- problem36.rb
+++ problem36.rb
@@ -31,3 +31,13 @@ reverse_numbers.each do |j|
 end
 
 check_numbers.sum == 872187
+
+# def palindrome? s
+# 	s == s.reverse
+# end
+
+# sum = (1..1_000_000).inject(0) do |sum, n|
+# 	sum += palindrome?(n.to_s) && palindrome?(n.to_s 2) ? n : 0
+# end
+
+# puts sum
