0e694f829a825985b1065c83c7e2a28350701f4d
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-10 09:20:02 +0900

    [add]problem39 直角三角形の辺の合計の最大値

--- problem39.rb
+++ problem39.rb
@@ -5,12 +5,21 @@
 
 # p ≤ 1000 のとき解の数が最大になる p はいくつか?
 
-array = (1..400).to_a
+# array = (1..100).to_a
 
-answer = 0
+# answer = []
+# array.repeated_combination(3).each do |a,b,c|
+#   answer << [[a,b,c].sum,[a,b,c]] if a**2 + b**2 == c **2 && a+b+c <= 1000
+# end
+
+# p answer.sort.map{|a| a[0]}
+
+
+array = (1..1000).to_a
+
+answer = []
 array.repeated_combination(3).each do |a,b,c|
-  answer = [a,b,c].sum if a**2 + b**2 == c **2 && answer < [a,b,c].sum && a+b+c <= 1000
-  p [a,b,c] if [a,b,c].sum == 960 &&  a**2 + b**2 == c **2 
+  answer << [[a,b,c].sum,[a,b,c]] if a**2 + b**2 == c **2 && a+b+c <= 1000
 end
 
-p answer
+p answer.sort.map{|a| a[0]}.tally.to_a.sort {|a,b| a[1] <=> b[1]}.reverse.first[0]
